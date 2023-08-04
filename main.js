e73e2b3eac71dd325e6203ab2a2eca44a92aca9b
Aayushraj1999 <ayushraj109@gmail.com>
2023-08-04 19:36:36 +0530

    make head and all hello world before item lister

--- main.js
+++ main.js
@@ -24,12 +24,12 @@
 
 //GETELEMENTSBYCLASSNAME //
 
-var items= document.getElementsByClassName('list-group-item');
+//var items= document.getElementsByClassName('list-group-item');
 //console.log(items);
 //console.log(items[1]);
 //items[1].textContent="hello 2";
 //items[1].style.fontWeight="bold";
-items[1].style.backgroundColor ='green';
+//items[1].style.backgroundColor ='green';
 //items[2].style.backgroundColor ='grey';
 
 //for(var i =0; i<items.length;i++){
@@ -76,18 +76,106 @@ items[1].style.backgroundColor ='green';
 
 // queryselectorall //
 
- var titles = document.querySelectorAll('.title');
+ //var titles = document.querySelectorAll('.title');
 
- console.log(titles);
- titles[0].textContent ='Hello';
+ //console.log(titles);
+ //titles[0].textContent ='Hello';
 
- var odd = document.querySelectorAll('li:nth-child(odd)');
+ //var odd = document.querySelectorAll('li:nth-child(odd)');
 
- for(var i =0; i <odd.length; i++){;
- odd[i].style.backgroundColor ="green";
- }
+ //for(var i =0; i <odd.length; i++){;
+ //odd[i].style.backgroundColor ="green";
+ //}
 
 
  //var items = document.querySelectorAll([items[2]]);
   //items[2].styel.fontColor = 'green';
   //console.log(items[2])
+
+
+  //TRAVERSING THE DOM //
+
+  var itemList = document.querySelector('#items');
+  //parentNode 
+
+  //console.log(itemList.parentNode);
+  //itemList.parentNode.style.backgroundColor = '#f4f4f4';
+  //console.log(itemList.parentNode.parentNode.parentNode);
+
+  //parentElement //
+
+  //console.log(itemList.parentElement);
+  //itemList.parentElement.style.backgroundColor = '#f4f4f4';
+  //console.log(itemList.parentElement.parentElement.parentElement);
+
+
+  //childNodes//
+//console.log(itemList.childNodes);
+
+//console.log(itemList.children);
+//console.log(itemList.children[1]);
+//itemList.children[1].style.backgroundColor ='yellow';
+
+//firstChild //
+
+//console.log(itemList.firstChild);
+
+//firstElementChild
+//console.log(itemList.firstElementChild);
+//itemList.firstElementChild.textContent='hello 1 '
+
+
+//lastChild
+
+//console.log(itemList.lastChild);
+
+//lastElementChild//
+//console.log(itemList.lasttElementChild);
+//itemList.lastElementChild.textContent='hello 4'
+
+//siblings //
+
+//console.log(itemList.nextSibling);
+
+//nextElementSibling 
+//console.log(itemList.nextElementSibling);
+
+//previousSibling
+//console.log(itemList.previousSibling);
+
+//previousElementSibling 
+//console.log(itemList.previousElementSibling);
+//itemList.previousElementSibling.style.color = 'green';
+
+//createElement 
+
+//create a div 
+var newDiv =document.createElement('div');
+
+//add class
+newDiv.className ='hello';
+
+//add id
+newDiv.id ='hello1';
+
+//add attribute
+newDiv.setAttribute('title','hello Div');
+
+//create text node
+var newDivText =document.createTextNode('hello world')
+
+//add text to div
+
+newDiv.appendChild(newDivText);
+
+
+var container = document.querySelector('header .container');
+var h1 =document.querySelector('header h1');
+var h2 = document.querySelector('header h2');
+
+
+console.log(newDiv);
+
+newDiv.style.fontSize ='30px'
+
+container.insertBefore(newDiv ,h1);
